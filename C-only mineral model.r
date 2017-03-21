5cbb178b23d4409c2f5ba845d72e6fd70d25289c
Colin Averill <caverill@pecan-test.bu.edu>
2017-03-21 19:44:17 -0400

    Added transfers of POM -> MAOM (C->M).

--- C-only mineral model.r
+++ C-only mineral model.r
@@ -1,26 +1,27 @@
 #making the simplest mineral POM and MAOM model I can.
 #No input sensitivity of POM, as suspected. Increasing inputs increasing MBC. 
 #This also increases MAOM, as we increase the input rate at steady state, even with non-linear desorption kinetics.
-
 #sorption follows langmuir isotherm adsorption model, only microbial biomass turnover enters the sorbed pool. 
+#There is also potential for some fraction of POM to enter the MAOM pool directly, rather than passing through MB.
 
 #clear R environment.
 rm(list=ls())
 
 #parameters
-I    <- 1        #C input rate                        (mg time-1)
+I    <- 1.0      #C input rate                        (mg time-1)
 CUE  <- 0.3      #carbon use efficiency               (unitless)
 v1   <- .4       #biomass-specific decay multiplier   (mg time-1)
-v2   <- 2        #Vmax of sorption                    (mg time-1)
+v2   <- 1        #Vmax of sorption                    (mg time-1)
 k1   <- 100      #half saturation of decomp           (mg)
 k2   <- 10       #half saturation of sorption         (mg)
 h1   <- 0.05     #biomass turnover rate               (1/time)
 h2   <- 0.002    #C-specific desorption rate          (1/time)
+h3   <- 0.001    #fraction of POM that potentially sorbs (1/time)
 
 #initial pool sizes
 C <- 100       #POM               (mg / g)
 M <-  20       #MAOM              (mg / g)
-B <- C*0.1    #microbial biomass (mg / g)
+B <- C*0.1    #microbial biomass  (mg / g)
 
 #number of days to step the dynamic simulation through time.
 t <- 1500
@@ -32,14 +33,17 @@ out <- matrix(rep(0,t*length(outputs)),nrow=t,dimnames=list(NULL,outputs))
 
 for(i in 1:t){
   #defining fluxes
-  SORPTION <- v2*h1*B / (k2 + h1*B)
+  DEATH      <- h1*B
+  DECOMP     <- v1*B*C / (k1 + C)
+  POM2MOM    <- h3*C
+  SORPTION   <- v2*(POM2MOM+DEATH) / (k2 + POM2MOM+DEATH)
+  SORPTION.B <- (DEATH  /(POM2MOM+DEATH))*SORPTION
+  SORPTION.P <- (POM2MOM/(POM2MOM+DEATH))*SORPTION
   DESORPTION <- h2*M
-  DEATH <- h1*B
-  DECOMP <- v1*B*C / (k1 + C)
   
   #ODEs
   dBdt <- CUE * DECOMP - DEATH
-  dCdt <- I + (DEATH - SORPTION) + DESORPTION - DECOMP
+  dCdt <- I + (DEATH - SORPTION.B) + DESORPTION - DECOMP - SORPTION.P
   dMdt <- SORPTION - DESORPTION
   
   #update pools
@@ -62,8 +66,8 @@ require(rootSolve)
 model<- function(t,y,pars){
   with(as.list(c(y,pars)),{
     dBdt <- CUE * v1 * B * C / (k1 + C) - h1*B
-    dCdt <- I + (h1*B - v2*h1*B / (k2 + h1*B)) + h2*M - v1 * B * C / (k1 + C)
-    dMdt <- v2*h1*B / (k2 + h1*B) - h2*M
+    dCdt <- I + (h1*B - (h1*B  /(h3*C+h1*B))*v2*(h3*C+h1*B) / (k2 + h3*C+h1*B)) + h2*M - v1 * B * C / (k1 + C) - (h3*C/(h3*C+h1*B))*v2*(h3*C+h1*B) / (k2 + h3*C+h1*B)
+    dMdt <- v2*(h3*C+h1*B) / (k2 + h3*C+h1*B) - h2*M
     list(c(dBdt,dCdt,dMdt))
   })
 }
@@ -74,4 +78,4 @@ y <- c(B=B,C=C,M=M)
 
 #numerically solve the model.
 ST <- stode(y=y,func=model,parms=pars,pos=T)
-ST
+ST$y; sum(ST$y)
